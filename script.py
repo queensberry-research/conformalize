494154eefba8172af26504cdf64b2daed9757bb4
Derek Wan <d.wan@icloud.com>
2026-01-12 14:37:13 +0000

    Change `PYPI_GITEA_READ_TOKEN` to be the actual token (#40)
    Reviewed-on: https://gitea.main:3000/qrt/conformalize/pulls/40
    Co-authored-by: Derek Wan <d.wan@icloud.com>
    Co-committed-by: Derek Wan <d.wan@icloud.com>

--- script.py
+++ script.py
@@ -45,12 +45,12 @@ if TYPE_CHECKING:
     from tomlkit.items import Table
 
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 LOGGER = getLogger(__name__)
 API_PACKAGES_QRT_PYPI = "api/packages/qrt/pypi"
 ACTION_TOKEN = "${{secrets.ACTION_TOKEN}}"  # noqa: S105
 PYPI_GITEA_USERNAME = "qrt-bot"
-PYPI_GITEA_READ_TOKEN = "${{secrets.PYPI_GITEA_READ_TOKEN}}"  # noqa: S105
+PYPI_GITEA_READ_TOKEN = "e43d1df41a3ecf96e4adbaf04e98cfaf094d253e"  # noqa: S105
 PYPI_GITEA_READ_WRITE_TOKEN = "${{secrets.PYPI_GITEA_READ_WRITE_TOKEN}}"  # noqa: S105
 PYPI_NANODE_USERNAME = "qrt"
 PYPI_NANODE_PASSWORD = "${{secrets.PYPI_NANODE_PASSWORD}}"  # noqa: S105
