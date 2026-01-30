11b45d838b8abb0afdd33f6d186b88ad210cf498
Derek Wan <derek@queensberryresearch.com>
2026-01-30 12:31:54 +0000

    Update logging to simpler `set_up_logging` (#82)
    Co-authored-by: github-actions-bot <noreply@github.com>
    Reviewed-on: https://gitea.main:3000/qrt/pre-commit-hooks/pulls/82

--- src/qrt_pre_commit_hooks/__init__.py
+++ src/qrt_pre_commit_hooks/__init__.py
@@ -3,14 +3,13 @@ from __future__ import annotations
 import sys
 
 from utilities.constants import MONTH
-from utilities.core import is_debug
-from utilities.logging import setup_logging
+from utilities.core import is_debug, set_up_logging
 from utilities.traceback import make_except_hook
 
-__version__ = "0.3.25"
+__version__ = "0.3.26"
 
 
-setup_logging(__name__, files_dir=".logs")
+set_up_logging(__name__, files=".logs")
 sys.excepthook = make_except_hook(
     path_max_age=MONTH, path=".logs/errors", version=__version__, pudb=is_debug
 )
